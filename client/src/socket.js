9495c7ec426e2d7b93ff0d314edf4dfe9a127895
Angel OJeda <92764780+IngAngelOjeda@users.noreply.github.com>
2026-03-20 00:12:02 -0300

    Use environment variable for server URL in socket configuration

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io({
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || '';
+
+const socket = io(SERVER_URL, {
   autoConnect: false,
   transports: ['websocket', 'polling'],
 });
